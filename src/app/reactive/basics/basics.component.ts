9975692f23d8e6515c22d472f677c258df52be54
santiago.moreno@geoaitech.com <santiago.moreno@geoaitech.com>
2023-03-10 14:19:57 -0500

    add angular foms reactive

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styleUrls: ['./basics.component.css']
+  styleUrls: ['./basics.component.css'],
 })
-export class BasicsComponent {
+export class BasicsComponent implements OnInit {
+  // myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('RTX-4080I'),
+  //   price: new FormControl(1500),
+  //   stock: new FormControl(5),
+  // });
 
+  myForm: FormGroup = this.fb.group({
+    name: [, [Validators.required, Validators.minLength(3)]],
+    price: [, [Validators.required, Validators.min(0)]],
+    stock: [, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    // it is not mandatory to wait for the same form object
+    this.myForm.reset({
+      name: 'RTX-4080I',
+      price: 1500,
+    });
+
+    // it is mandatory to wait for the same form object
+    // this.myForm.setValue({
+    //   name: 'RTX-4080I',
+    //   price: 1500,
+    //   stock: 12
+    // })
+  }
+
+  fieldNoValid(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  saveForm() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('form', this.myForm.value);
+    this.myForm.reset();
+  }
 }
