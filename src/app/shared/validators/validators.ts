b8bc7045df179ed63f6cdbd83c6842f3e04d9cd3
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-03 18:30:28 -0500

    added email validators

---
+++ src/app/shared/validators/validators.ts
@@ -0,0 +1,9 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+export const cantBeStrider = (control: FormControl): ValidationErrors | null => {
+    const value: string = control.value.trim().toLowerCase();
+    return value == "strider" ? { noStrider: true } : null
+}
