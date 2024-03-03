43bc3edcebf1c63d9dbb410e0162e3636b4d6445
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-03 18:31:39 -0500

    added emailValidator service

---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, delay, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+
+export class emailValidator implements AsyncValidator {
+    validate(control: AbstractControl): Observable<ValidationErrors | null> {
+        const email = control.value;
+        const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+            if (email === "fernando@google.com") {
+                subscriber.next({ emailTaken: true });
+                subscriber.complete();
+                // return;
+            };
+
+            subscriber.next(null);
+            subscriber.complete();
+        }).pipe(
+            delay(3000)
+        );
+
+        return httpCallObservable;
+    }
+
+    // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    //     const email = control.value;
+    //     console.log(email)
+    //     return of({
+    //         emailTaken: true
+    //     }).pipe(
+    //         delay(2000)
+    //     );
+    // }
+}
