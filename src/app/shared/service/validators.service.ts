bca7d4cace578409aa1991024e75df5432394206
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-03 18:31:26 -0500

    added validators service

---
+++ src/app/shared/service/validators.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+
+export class validatorsService {
+    public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+    public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+    public cantBeStrider = (control: FormControl): ValidationErrors | null => {
+        const value: string = control.value.trim().toLowerCase();
+        return value == "strider" ? { noStrider: true } : null
+    }
+
+    public isValidField(form: FormGroup, field: string) {
+        return form.controls[field].errors && form.controls[field].touched;
+    }
+
+    public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+        return (formGroup: AbstractControl): ValidationErrors | null => {
+            const fieldValue1 = formGroup.get(field1)?.value;
+            const fieldValue2 = formGroup.get(field2)?.value;
+
+            if (fieldValue1 !== fieldValue2) {
+                formGroup.get(field2)?.setErrors({ notEqual: true });
+                return { notEqual: true }
+            }
+
+            formGroup.get(field2)?.setErrors(null);
+            return null;
+        }
+    }
+}
